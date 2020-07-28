6319c5197620521709122f9e8330a6d2f774e770
tanweeralii <tanweeralii908@gmail.com>
2020-07-28 16:07:52 +0530

    Updated variables

--- scripts/__index.js
+++ scripts/__index.js
@@ -7,9 +7,9 @@ function list_running_containers(){
 	    var obj = JSON.parse(req);
 	    var message = JSON.parse(obj.message);
 	    if(message==""){
-	        const row = document.createElement('row');
+	        const para = document.createElement('p');
 	        row.textContent = "No Container is alive";
-	        result_area.appendChild(row);
+	        result_area.appendChild(para);
 		    result_area.style.color = "white";
 	    }
 	    else{
@@ -25,21 +25,21 @@ function list_running_containers(){
 }
 function list_available_images(){
 	$.post('list_available_images',function(req,res){
-		hello = document.getElementById('ans');
-		while(hello.firstChild){
-		    hello.removeChild(hello.firstChild);
+		result_area = document.getElementById('ans');
+		while(result_area.firstChild){
+		    result_area.removeChild(result_area.firstChild);
 		}
 	    var obj = JSON.parse(req);
-	    var obj1 = JSON.parse(obj.message);
-	    const p = document.createElement('p');
-	    p.textContent = "Example: If output is 'xyz:latest , sha256:b65575bf92725cad124e7c0d35d479ee23a3281cf066dcaba86c311bbe5e0e31', then the image name is 'xyz' and image id is first 12 digit of sha256 i.e., b65575bf9272 in this case";
-	    hello.appendChild(p);
-        obj1.forEach((message) => {
+	    var message = JSON.parse(obj.message);
+	    const para = document.createElement('p');
+	    para.textContent = "Example: If output is 'xyz:latest , sha256:b65575bf92725cad124e7c0d35d479ee23a3281cf066dcaba86c311bbe5e0e31', then the image name is 'xyz' and image id is first 12 digit of sha256 i.e., b65575bf9272 in this case";
+	    hello.appendChild(para);
+        message.forEach((message) => {
 			const li = document.createElement('li');
             li.textContent = (message.RepoTags)[0] + " , " + message.Id;
-           	hello.appendChild(li);
+           	result_area.appendChild(li);
         })
-        hello.style.color = "white";
+        result_area.style.color = "white";
 	});
 }
 function list_all_containers(){
@@ -49,8 +49,8 @@ function list_all_containers(){
 	        hello.removeChild(hello.firstChild);
 	    }
 	    var obj = JSON.parse(req);
-	    var obj1 = JSON.parse(obj.message);
-       	obj1.forEach((message) => {
+	    var message = JSON.parse(obj.message);
+       	message.forEach((message) => {
         	console.log(message);
 		    const li = document.createElement('li');
 		    var i = message.Id.slice(0, 12).trim();
