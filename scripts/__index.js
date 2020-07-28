eef3a486d11d7a7f7ba733b85a9175506a93cc89
tanweeralii <tanweeralii908@gmail.com>
2020-07-28 16:14:36 +0530

    Updated variable names

--- scripts/__index.js
+++ scripts/__index.js
@@ -44,80 +44,80 @@ function list_available_images(){
 }
 function list_all_containers(){
 	$.post('list_all_containers',function(req,res){
-		hello = document.getElementById('ans');
-		while(hello.firstChild){
-	        hello.removeChild(hello.firstChild);
+		result_area = document.getElementById('ans');
+		while(result_area.firstChild){
+	        result_area.removeChild(result_area.firstChild);
 	    }
 	    var obj = JSON.parse(req);
 	    var message = JSON.parse(obj.message);
        	message.forEach((message) => {
         	console.log(message);
 		    const li = document.createElement('li');
-		    var i = message.Id.slice(0, 12).trim();
-           	li.textContent = message.Image + " , " + i;
-            hello.appendChild(li);
+		    var result = message.Id.slice(0, 12).trim();
+           	li.textContent = message.Image + " , " + result;
+            result_area.appendChild(li);
         })
-        hello.style.color = "white";
+        result_area.style.color = "white";
 	});
 }
 function stop_container(){
 	var id = document.getElementById('input1').value;
 	document.getElementById('input1').value = "";
 	$.post('stop_container',{'id':id},function(req,res){
-		hello = document.getElementById('ans');
-		while(hello.firstChild){
-			hello.removeChild(hello.firstChild);
+		result_area = document.getElementById('ans');
+		while(result_area.firstChild){
+			result_area.removeChild(result_area.firstChild);
 		}
-	    const p = document.createElement('p');
-	    p.textContent = "Done";
-	    hello.appendChild(p);
-		hello.style.color = "white";
+	    const para = document.createElement('p');
+	    para.textContent = "Done";
+	    result_area.appendChild(para);
+		result_area.style.color = "white";
 	});
 }
 function start_container(){
 	var id = document.getElementById('input2').value;
 	document.getElementById('input2').value = "";
 	$.post('start_conatiner',{'id':id},function(req,res){
-		hello = document.getElementById('ans');
-		while(hello.firstChild){
-		    hello.removeChild(hello.firstChild);
+		result_area = document.getElementById('ans');
+		while(result_area.firstChild){
+		    result_area.removeChild(result_area.firstChild);
 		}
-	    const p = document.createElement('p');
-		p.textContent = "Done";
-	    hello.appendChild(p);
-        hello.style.color = "white";
+	    const para = document.createElement('p');
+		para.textContent = "Done";
+	    result_area.appendChild(para);
+        result_area.style.color = "white";
 	});
 }
 function restart_container(){
 	var id = document.getElementById('input3').value;
 	document.getElementById('input3').value = "";
 	$.post('restart_container',{'id':id},function(req,res){
-		hello = document.getElementById('ans');
-		while(hello.firstChild){
-		    hello.removeChild(hello.firstChild);
+		result_area = document.getElementById('ans');
+		while(result_area.firstChild){
+		    result_area.removeChild(result_area.firstChild);
 		}
-	    const p = document.createElement('p');
-	    p.textContent = "Done";
-	    hello.appendChild(p);
-        hello.style.color = "white";
+	    const para = document.createElement('p');
+	    para.textContent = "Done";
+	    result_area.appendChild(para);
+       	result_area.style.color = "white";
 	});
 }
 function container_logs(){
 	var id = document.getElementById('input4').value;
 	document.getElementById('input4').value = "";
 	$.post('get_container_logs',{'id':id},function(req,res){
-		hello = document.getElementById('ans');
-		while(hello.firstChild){
-			hello.removeChild(hello.firstChild);
+		result_area = document.getElementById('ans');
+		while(result_area.firstChild){
+			result_area.removeChild(result_area.firstChild);
 		}
 	    var obj = JSON.parse(req);
 	    var res = obj.message.split("exit");
 	    res.forEach((message) => {
 			const li = document.createElement('li');
-			var res1 = message.split("exit");
-            li.textContent = res1;
-            hello.appendChild(li);
-        	hello.style.color = "white";
+			var result = message.split("exit");
+            li.textContent = result;
+            result_area.appendChild(li);
+        	result_area.style.color = "white";
 		});
 	});
 }
