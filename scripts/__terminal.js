e9abe752e4c2aefc3af0f6b597faec4cd359124b
tanweeralii <tanweeralii908@gmail.com>
2020-08-04 15:55:54 +0530

    Completed web tty

--- scripts/__terminal.js
+++ scripts/__terminal.js
@@ -1,12 +1,23 @@
 $('body').terminal(function(command){
     if (command !== '') {
         var container_id = sessionStorage.getItem("DockerID");
-        $.post('terminal_command',{'command':command, 'container_id':container_id},function(req,res){
-            console.log(req);
-            var obj = JSON.parse(req);
-            var object = JSON.parse(obj.message);
-            this.echo("[[gb;green;black]" + object + "]");
-        })
+        var result = function () {
+            var tmp = null;
+            $.ajax({
+                'async': false,
+                'type': "POST",
+                'global': false,
+                'dataType': 'html',
+                'url': 'terminal_command',
+                'data': {'command':command, 'container_id':container_id},
+                'success': function (data) {
+                    tmp = data;
+                }
+            });
+            return tmp;
+        }();
+        var result_final = JSON.parse(result);
+        this.echo("[[gb;green;black]" + result_final.message + "]");
     }
 }, {
         greetings: 'Get access to your Docker terminal',
